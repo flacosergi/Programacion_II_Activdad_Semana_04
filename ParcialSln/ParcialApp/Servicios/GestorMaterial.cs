dd771dacad0c6d82f4966eeb759777ddefe06122
flacosergi <44148978+flacosergi@users.noreply.github.com>
2023-10-06 18:58:43 -0300

    Add files via upload

---
+++ ParcialSln/ParcialApp/Servicios/GestorMaterial.cs
@@ -0,0 +1,22 @@
+﻿using ParcialApp.Acceso_a_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParcialApp.Dominio;
+
+
+namespace ParcialApp.Servicios
+{
+    public class GestorMaterial
+    {
+        private IDaoMaterial Dao = new DaoMaterial();
+
+        public List<Material> CargarMateriales()
+        {
+            return Dao.GetMateriales();
+        }
+
+    }
+}
---
+++ ParcialSln/ParcialApp/Servicios/GestorOrden.cs
@@ -0,0 +1,28 @@
+﻿using ParcialApp.Acceso_a_datos;
+using ParcialApp.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialApp.Servicios
+{
+    public class GestorOrden
+    {
+
+        private IDaoOrden dao_orden;
+
+        public GestorOrden()
+        {
+            dao_orden = new DaoOrden();
+        }
+
+        public int CargarOrden(Orden_Retiro orden)
+        {
+            return dao_orden.GuardaOrden(orden);
+
+        }
+       
+    }
+}
